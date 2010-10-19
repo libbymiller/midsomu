67f44fda73797f900c919befab755c4fbee52e51
Libby Miller <libby@nicecupoftea.org>
2010-10-19 13:35:13 +0100

    Fixed the problem with presence updates, thanks to Danbri's idea of using anonymous logins. TestServlet is no longer used.

--- collector.rb
+++ collector.rb
@@ -9,13 +9,11 @@ class Collector
   attr_reader :client, :roster
 
   def initialize
-    self.jid = "collector@jabber.notu.be"
+    self.jid = "collector6@jabber.notu.be"
     self.password = "foobar"
     @client = Client.new(self.jid)
-    self.options = []
+    self.options = ['Joyce','Tom','Posh Man']
     self.hashtag = ""
-    self.counts ={ "Joyce"=> 9, "John"=> 1, "Giles"=> 0, "Grace"=> 1, "Teddy Molloy"=> 0, "Iris"=> 0, "Ronnie"=> 0, "Tom"=> 6, "Libby"=> 1, "Sebastian"=> 0, "Ken"=> 0, "Frank"=> 0, "Camilla"=> 1, "Gerald"=> 6 }
-    self.images={ "Joyce"=> "http://dev.notu.be/2010/10/mm/images/joyce.jpg", "Giles"=> "http://www.macfarlane-chard.co.uk/files/clients/BonesKen.jpg", "Tom Seleck"=> "http://tomselleck.tv-website.com/images/pic_selleck.jpg", "Grace"=> "http://www.bbc.co.uk/pressoffice/images/bank/programmes_tv/drama/sinchronicity/300camille.jpg", "Posh Man"=> "http://dev.notu.be/2010/10/mm/images/unknown_man_1.gif", "Iris"=> "http://media.ove.cybermage.se/2010/03/Michelle-Fairley-2.jpg", "Teddy Molloy"=> "http://static.episode39.it/artist/3224.jpg", "Tom"=> "http://dev.notu.be/2010/10/mm/images/barnaby.jpg", "Libby"=> "http://thm-a01.yimg.com/nimage/6f3f771a10cf63fa", "Ken"=> "http://www.carrieswar.com/images/Daniel.jpg", "Camilla"=> "http://www.radiotimes.com/shows/torchwood/cast/daniela-denby-ashe%20/img.jpg", "Gerald"=> "http://3.bp.blogspot.com/_RaOrchOImw8/StdrWx0iicI/AAAAAAAAZys/cq-wNz3RufU/s400/Kevin+R+McNally.jpg", "Frank"=> "http://radaris.com/imdb/8/1/8156ed21c571b627e129f93718abe98f.jpg" }
     self.options.each do |o|
       self.counts[o]=0
     end
@@ -29,17 +27,11 @@ class Collector
     @client.auth(@password)
     @client.send(Presence.new.set_type(:available))
 
-    #the "roster" is our bot contact list
     @roster = Roster::Helper.new(@client)
 
-    #start_presence_callback
     start_subscription_request_callback
 
-    #...to do something with the messages we receive
     start_message_callback
-
-    # doesn't do anything useful as yet
-    #start_presence_sending
 
   end
 
@@ -50,6 +42,8 @@ class Collector
         status = "{'counts':"+JSON.pretty_generate(counts)+",'images':"+JSON.pretty_generate(images)+",'hashtag':'"+self.hashtag+"'}"
         new_msg = Message::new(jid,status)
         new_msg.type=:chat
+        # just this one time we do a chat, therest of teh time we waity for presence
+        # this is because it takes  little while to sort itself out it seems, the first time
         @client.send(new_msg)
       end
       if(msg.body.match("do_vote"))
@@ -59,29 +53,12 @@ class Collector
         r = JSON.parse(msg.body)
         b = r["do_vote"]
         c = counts[b]
-        puts "got a count for #{b} which is #{c}"
-        
+        puts "got a count for #{b} which is #{c}"        
         d = c+1
         counts[b]=d
-        # give back the totals
-        fr_jid = msg.from
-        status = "{'counts':"+JSON.pretty_generate(counts)+",'images':"+JSON.pretty_generate(images)+",'hashtag':'"+self.hashtag+"'}"
-        new_msg = Message::new(fr_jid,status)
-        new_msg.type=:chat
-        @client.send(new_msg)
       end
 
-      if(msg.body.match("do_refresh"))
-        puts "sending refresh response #{msg.from}"
-        # give back the totals
-        fr_jid = msg.from
-        status = "{'counts':"+JSON.pretty_generate(counts)+",'images':"+JSON.pretty_generate(images)+",'hashtag':'"+self.hashtag+"'}"
-        new_msg = Message::new(fr_jid,status)
-        new_msg.type=:chat
-        @client.send(new_msg)
-      end
-
-###admin stuff
+# admin options
       if(msg.body.match("do_add_option"))
          puts "adding option"
          r = JSON.parse(msg.body)
@@ -95,12 +72,6 @@ class Collector
            if(ii)
              self.images[bn]=ii
            end
-           puts "count is #{counts[bn]} #{options.size}"
-           fr_jid = msg.from
-           status = "{'counts':"+JSON.pretty_generate(counts)+",'images':"+JSON.pretty_generate(images)+",'hashtag':'"+self.hashtag+"'}"
-           new_msg = Message::new(fr_jid,status)
-           new_msg.type=:chat
-           @client.send(new_msg)
          else
            puts "no name found"
          end
@@ -120,29 +91,13 @@ class Collector
          b = r["do_set_hashtag"]
          self.hashtag = b
       end
+      # update presence for everything and everyone (tooo much?)
 
+      status = "{'counts':"+JSON.pretty_generate(counts)+",'images':"+JSON.pretty_generate(images)+",'hashtag':'"+self.hashtag+"'}"
+      p = Jabber::Presence.new(:chat,status).set_type(:available)
+      @client.send(p)
     end
 
-  end
-
-  def start_presence_sending
-    @roster.wait_for_roster
-    t1 = Thread.new do
-      count = 0
-      while(true)
-        count = count +1
-        sleep(10)
-        status = "{'counts':"+JSON.pretty_generate(counts)+",'images':"+JSON.pretty_generate(images)+",'hashtag':"+self.hashtag+"}"
-        p = Jabber::Presence.new(:chat,status).set_type(:available)
-        @client.send(p)
-      end
-    end
-  end
-
-  def start_presence_callback
-    @roster.add_presence_callback do |item,pres|
-      puts "pres!!!!!!! #{pres}"
-    end
   end
 
   def start_subscription_request_callback
